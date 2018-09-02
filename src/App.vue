e471e56ec2f05dc66b6b6e9c1b16559dca93e590
Evgeny Belkin <evbelkin@localhost.localdomain>
2018-09-02 23:23:35 +0300

    корневой компонент

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <template>
-  <router-view />
+  <b-container fluid>
+    <router-view :user="user" />
+  </b-container>
 </template>
 
 <script>
+import bContainer from "bootstrap-vue/es/components/layout/container";
 
 export default {
-  name: 'app'
-}
+  components: {
+    "b-container": bContainer
+  },
+  created() {
+    this.getUserInfo();
+  },
+  data() {
+    return {
+      user: {}
+    };
+  },
+  methods: {
+    getUserInfo() {
+      return fetch("/user/get-info")
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Ошибка получения сведений о пользователе!");
+          }
+        })
+        .then(result => {
+          this.user = result.userData;
+        })
+        .catch(err => err);
+    }
+  }
+};
 </script>
