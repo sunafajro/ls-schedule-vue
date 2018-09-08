e5ad3b7b1be9fc235ed29100ebb7abefe4215b64
Evgeny Belkin <sunafajro@gmail.com>
2018-09-08 21:33:26 +0300

    доработано заполнение формы перед отправкой

--- src/components/CreateContent.vue
+++ src/components/CreateContent.vue
@@ -2,20 +2,20 @@
   <b-col sm="12" md="10" lg="10" xl="10">
     <form>
         <b>Преподаватель:</b>
-        <b-form-select class="schedule-create-form-margin" :options="optionsTeacher" size="sm" v-model="selectedTeacher" />
-        <b>Услуга:</b>
-        <b-form-select class="schedule-create-form-margin" :disabled="!selectedTeacher" :options="optionsService" size="sm" v-model="selectedService" />
+        <b-form-select class="schedule-create-form-margin" @change="getTeacherGroups" :options="optionsTeacher" size="sm" v-model="selectedTeacher" />
+        <b>Группа:</b>
+        <b-form-select class="schedule-create-form-margin" :disabled="!groups.length" :options="optionsGroup" size="sm" v-model="selectedGroup" />
         <b>Офис:</b>
-        <b-form-select class="schedule-create-form-margin" :options="optionsOffice" size="sm" v-model="selectedOffice" />
+        <b-form-select class="schedule-create-form-margin" @change="getOfficeRooms" :options="optionsOffice" size="sm" v-model="selectedOffice" />
         <b>Кабинет:</b>
-        <b-form-select class="schedule-create-form-margin" :disabled="!selectedOffice" :options="optionsRoom" size="sm" v-model="selectedRoom" />
+        <b-form-select class="schedule-create-form-margin" :disabled="!rooms.length" :options="optionsRoom" size="sm" v-model="selectedRoom" />
         <b>Время начала:</b>
         <b-row>
           <b-col>
-            <b-form-select class="schedule-create-form-margin" :options="optionsHours" size="sm" v-model="startHour" />  
+            <b-form-select class="schedule-create-form-margin" @change="adjustEndHour" :options="optionsHours" size="sm" v-model="startHour" />  
           </b-col>
           <b-col>
-            <b-form-select class="schedule-create-form-margin" :options="optionsMinutes" size="sm" v-model="startMinute" />
+            <b-form-select class="schedule-create-form-margin" @change="adjustEndMinute" :options="optionsMinutes" size="sm" v-model="startMinute" />
           </b-col>
         </b-row>
         <b>Время конца:</b>
@@ -35,13 +35,16 @@
 </template>
 
 <script>
+import moment from "moment";
 import bButton from "bootstrap-vue/es/components/button/button";
 import bCol from "bootstrap-vue/es/components/layout/col";
 import bFormSelect from "bootstrap-vue/es/components/form-select/form-select";
 import bRow from "bootstrap-vue/es/components/layout/row";
-import moment from "moment";
-
-moment.locale("ru");
+import {
+  createDaysSelectItems,
+  createHoursSelectItems,
+  createMinutesSelectItems
+} from "../utils";
 
 export default {
   components: {
@@ -52,43 +55,22 @@ export default {
   },
   computed: {
     optionsDay() {
-      const days = [{
-        value: null,
-        text: '-выбрать-'
-      }];
-      for(let i = 1; i < 8; i++) {
-        days.push({
-          value: i,
-          text: moment().day(i).format('dddd')
-        });
-      }
-      return days;
+      const options = [{ value: null, text: "-выбрать-" }].concat(
+        createDaysSelectItems()
+      );
+      return options;
     },
     optionsHours() {
-      const hours = [{
-        value: null,
-        text: '-часы-'
-      }];
-      for(let i = 8; i < 20; i++) {
-        hours.push({
-          value: i < 10 ? `0${i}` : i,
-          text: i < 10 ? `0${i}` : i
-        });
-      }
-      return hours;
+      const options = [{ value: null, text: "-часы-" }].concat(
+        createHoursSelectItems()
+      );
+      return options;
     },
     optionsMinutes() {
-      const minutes = [{
-        value: null,
-        text: '-минуты-'
-      }];
-      for(let i = 0; i < 60; i = i + 5) {
-        minutes.push({
-          value: i < 10 ? `0${i}` : i,
-          text: i < 10 ? `0${i}` : i,
-        });
-      }
-      return minutes;
+      const options = [{ value: null, text: "-минуты-" }].concat(
+        createMinutesSelectItems()
+      );
+      return options;
     },
     optionsOffice() {
       const options = [{ value: null, text: "-выбрать-" }].concat(this.offices);
@@ -98,10 +80,8 @@ export default {
       const options = [{ value: null, text: "-выбрать-" }].concat(this.rooms);
       return options;
     },
-    optionsService() {
-      const options = [{ value: null, text: "-выбрать-" }].concat(
-        this.services
-      );
+    optionsGroup() {
+      const options = [{ value: null, text: "-выбрать-" }].concat(this.groups);
       return options;
     },
     optionsTeacher() {
@@ -115,19 +95,74 @@ export default {
     return {
       endHour: null,
       endMinute: null,
+      groups: [],
       rooms: [],
       selectedDay: null,
       selectedOffice: null,
-      selectedService: null,
+      selectedGroup: null,
       selectedRoom: null,
       selectedTeacher: null,
-      services: [],
       startHour: null,
-      startMinute: null,
+      startMinute: null
     };
   },
   methods: {
-    create() {}
+    create() {},
+    async getTeacherGroups(tid) {
+      try {
+        const { groupsData } = await fetch(
+          `/schedule/get-teacher-groups?tid=${tid}`
+        ).then(r => {
+          if (r.ok) {
+            return r.json();
+          } else {
+            throw new Error();
+          }
+        });
+        // модифицируем массив
+        const groups = groupsData.map(item => {
+          return {
+            value: item.value,
+            text: `#${item.value} ${item.text}`
+          };
+        });
+        this.groups = groups;
+      } catch (e) {
+        throw new Error("Ошибка получения групп преподавателя!");
+      }
+    },
+    async getOfficeRooms(oid) {
+      try {
+        const { roomsData } = await fetch(
+          `/room/index?type=bootstrap&oid=${oid}`
+        ).then(r => {
+          if (r.ok) {
+            return r.json();
+          } else {
+            throw new Error();
+          }
+        });
+        this.rooms = roomsData;
+      } catch (e) {
+        throw new Error("Ошибка получения кабинетов офиса!");
+      }
+    },
+    adjustEndHour(h) {
+      this.adjustEndTime(h, this.startMinute);
+    },
+    adjustEndMinute(m) {
+      this.adjustEndTime(this.startHour, m);
+    },
+    adjustEndTime(startHour, startMinute) {
+      if (startHour && startMinute) {
+        const startDate = moment();
+        startDate.set("hour", parseInt(startHour));
+        startDate.set("minute", parseInt(startMinute));
+        const endDate = startDate.add(1, "h");
+        this.endHour = endDate.get("hour") < 10 ? `0${endDate.get("hour")}` : String(endDate.get("hour"));
+        this.endMinute = endDate.get("minute") < 10 ? `0${endDate.get("minute")}` : String(endDate.get("minute"));
+      }
+    }
   },
   props: {
     offices: {
