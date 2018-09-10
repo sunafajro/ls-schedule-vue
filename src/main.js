b9d035367c22df61a62d6754c8589f599fe0c5be
Evgeny Belkin <sunafajro@gmail.com>
2018-09-10 21:56:10 +0300

    стили вынесены в один файл, подключена новая иконка

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCalendarAlt, faClock, faFilter, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCalendarAlt,
+  faClock,
+  faEraser,
+  faFilter,
+  faPlus
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "bootstrap/dist/css/bootstrap.css";
+import "./style.css";
 
-library.add(faCalendarAlt, faClock, faFilter, faPlus);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+library.add(faCalendarAlt, faClock, faEraser, faFilter, faPlus);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
