3356246c0f097bf0d831a493bebf23d5dda2e7b5
Evgeny Belkin <sunafajro@gmail.com>
2018-09-15 00:08:24 +0300

    изменен лейаут странички

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="container-fluid">
-    <c-breadcrumbs />
-    <router-view :user="user" />
+  <div class="page-wrapper">
+    <c-header />
+    <main role="main" class="container-fluid">
+      <c-breadcrumbs />
+      <router-view :user="user" />
+    </main>
+    <c-footer />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import { notify } from "./utils";
-import Breadcrumbs from "./components/Breadcrumbs.vue";
+import Breadcrumbs from "./common/Breadcrumbs.vue";
+import Footer from "./common/Footer.vue";
+import Header from "./common/Header.vue";
 
 export default {
   components: {
-    "c-breadcrumbs": Breadcrumbs
+    "c-breadcrumbs": Breadcrumbs,
+    "c-footer": Footer,
+    "c-header": Header
   },
   created() {
     this.getUserInfo();
---
+++ src/common/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <span class="text-muted">© Школа иностранных языков "Язык для Успеха" 2018</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
