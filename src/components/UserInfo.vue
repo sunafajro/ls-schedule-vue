74b5250bbbbb338e52f9e3bf89dc97864a0c1fc6
Evgeny Belkin <sunafajro@gmail.com>
2018-10-20 22:20:56 +0300

    исправление в стилях

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="well well-sm small" v-if="Object.keys(user).length">
-    <b class="text-display-block-element" v-if="!user.teacherId && user.name">{{ user.name }}</b>
-    <b class="text-display-block-element"  v-if="user.teacherId && user.name">
+    <b style="display: block" v-if="!user.teacherId && user.name">{{ user.name }}</b>
+    <b style="display: block"  v-if="user.teacherId && user.name">
       <a :href="'/teacher/view?id=' + user.teacherId">
         {{ user.name }}
       </a>
     </b>
-    <i class="text-display-block-element" v-if="user.role">{{ user.role }}</i>
-    <span class="text-display-block-element" v-if="user.roleId === '4'">{{ user.office }}</span>
+    <i style="display: block" v-if="user.role">{{ user.role }}</i>
+    <span style="display: block" v-if="user.roleId === '4'">{{ user.office }}</span>
   </div>
 </template>
 
